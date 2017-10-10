804e557e4f9259915bf39a907df7f77ca811feb5
Patricio <patricio89morales5@hotmail.com>
2017-10-10 11:01:33 +0200

    SupervisedData class + repo done.

--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/Model/SupervisedDataRepository.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/Model/SupervisedDataRepository.java
@@ -1,4 +1,7 @@
 package sensoremctrl.webapi.sensoremctrlapi.Model;
 
-public interface SupervisedDataRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import sensoremctrl.webapi.sensoremctrlapi.Table.SupervisedData;
+
+public interface SupervisedDataRepository extends JpaRepository<SupervisedData, Long> {
 }
--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/Table/SupervisedData.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/Table/SupervisedData.java
@@ -1,11 +1,43 @@
 package sensoremctrl.webapi.sensoremctrlapi.Table;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "SupervisedData")
 public class SupervisedData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long supervisedID;
+
+    public long getSupervisedID() {
+        return supervisedID;
+    }
+
+    public void setSupervisedID(long supervisedID) {
+        this.supervisedID = supervisedID;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    private int temperature;
+    private int humidity;
+
+
 
 
 }
