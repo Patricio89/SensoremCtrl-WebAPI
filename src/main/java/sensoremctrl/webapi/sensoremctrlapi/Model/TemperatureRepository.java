ba32f6c41530ffe6337a05397a13b9ff23d55510
Adrian <adde15w@hotmail.com>
2017-10-10 11:22:43 +0200

    ..no changes

--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/Model/TemperatureRepository.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/Model/TemperatureRepository.java
@@ -1,7 +1,13 @@
 package sensoremctrl.webapi.sensoremctrlapi.Model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sensoremctrl.webapi.sensoremctrlapi.Table.TemperatureLogg;
 
+import java.util.List;
+
+
 public interface TemperatureRepository extends JpaRepository<TemperatureLogg, Long> {
+
+
 }
--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/RequestManagment/TemperatureController.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/RequestManagment/TemperatureController.java
@@ -2,14 +2,9 @@ package sensoremctrl.webapi.sensoremctrlapi.RequestManagment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import sensoremctrl.webapi.sensoremctrlapi.Model.TemperatureRepository;
-import sensoremctrl.webapi.sensoremctrlapi.Table.TemperatureLogg;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/temperature")
@@ -17,6 +12,5 @@ public class TemperatureController {
 
     @Autowired
     private TemperatureRepository temperatureRepository;
-
 
 }
