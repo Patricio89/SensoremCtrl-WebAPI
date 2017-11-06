18114e8edcd31cbb9c9d305e85feab9bd52ef357
Adrian <adde15w@hotmail.com>
2017-11-06 11:05:17 +0100

    test return simple text again

--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/RequestManagment/TestClass.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/RequestManagment/TestClass.java
@@ -1,12 +1,13 @@
 package sensoremctrl.webapi.sensoremctrlapi.RequestManagment;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestClass {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String index() {
         return "Test av api";
     }
