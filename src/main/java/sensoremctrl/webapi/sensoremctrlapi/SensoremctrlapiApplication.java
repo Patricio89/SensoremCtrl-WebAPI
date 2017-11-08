69b54dd81d93a07da5e85ff38e03839b8362d217
Adrian <adde15w@hotmail.com>
2017-11-08 13:43:53 +0100

    Rest service packaged as war file, ready for deployment to azure

--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/SensoremctrlapiApplication.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/SensoremctrlapiApplication.java
@@ -2,16 +2,21 @@ package sensoremctrl.webapi.sensoremctrlapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class SensoremctrlapiApplication {
+public class SensoremctrlapiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SensoremctrlapiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SensoremctrlapiApplication.class, args);
 	}
+
 
 }
--- src/main/java/sensoremctrl/webapi/sensoremctrlapi/TestController.java
+++ src/main/java/sensoremctrl/webapi/sensoremctrlapi/TestController.java
@@ -2,8 +2,9 @@ package sensoremctrl.webapi.sensoremctrlapi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @RequestMapping("/")
